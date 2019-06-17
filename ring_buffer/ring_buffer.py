011f873fce187963ff083256ac816e7fd932a3d8
bcabanayan <bcabanayan@gmail.com>
2019-06-16 18:51:18 -0700

    Completed code for ring buffer data structure

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,12 +6,23 @@ class RingBuffer:
 
   def append(self, item):
     # use get to determine the number of elements in self.storage
+    num_elements = len(self.get())
     # if the number of elements is less than capacity...
-      # add the element to self.storage; number of elements = index to be inserted into
+    if num_elements < self.capacity:
+      # remove the element at index num_elements
+      self.storage.pop(num_elements)
+      # add the element to self.storage; insert at index num_elements
+      self.storage.insert(num_elements, item)
     # if the number of elements is equal to capacity...
+    elif num_elements == self.capacity:
+      # use self.current to keep track of number of elements added to the ringBuffer so far
+      insertion_index = self.current % self.capacity
       # remove the element from the head of self.storage
+      self.storage.pop(insertion_index)
       # add the new element to the tail of self.storage
-
+      self.storage.insert(insertion_index, item)
+    # keep count of elements added to ringbuffer by incrementing self.current
+    self.current += 1
 
   def get(self):
     # start with empty array
@@ -24,3 +35,4 @@ class RingBuffer:
     return elements
 
 print(RingBuffer(4).get())
+print(RingBuffer(4).append(1))
