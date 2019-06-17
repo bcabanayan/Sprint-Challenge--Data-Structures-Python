8acfed9dcec230fa89387a86a713baa3bc92a64d
bcabanayan <bcabanayan@gmail.com>
2019-06-16 20:00:17 -0700

    Added code to implement singly linked list

--- names/names.py
+++ names/names.py
@@ -11,12 +11,41 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+        
+# implementing solution with linked list
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def get_next(self):
+        return self.next
+  
+    def set_next(self, node):
+        self.next = node
+
+class LinkedList:
+  def __init__(self, head = None, tail = None):
+    self.head = head
+    self.tail = tail
+  
+  def add_to_tail(self, value):
+    new_tail = Node(value)
+    # point existing tail to new tail
+    if self.tail:
+        self.tail.set_next(new_tail)
+    # don't need to repoint new tail, already pointing to None
+    self.tail = new_tail
+
+print(LinkedList(None, None).add_to_tail(1))
+
+# end_time = time.time()
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime: {end_time - start_time} seconds")
 
